8ccd1c2b420e6042b91a8766ecc7979983a581df
Erick20320 <erickgarcia20320@gmail.com>
2023-06-18 18:54:35 -0600

    Implementando controlador para crear películas mediante solicitud POST

--- src/main/java/net/javaguides/springboot/controller/PeliculaController.java
+++ src/main/java/net/javaguides/springboot/controller/PeliculaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,12 @@ public class PeliculaController {
 	public List<Pelicula> getAllPeliculas() {
 		return peliculaRepository.findAll();
 	}
+	
+	//Crear Peliculas
+	
+	@PostMapping("/peliculas")
+	  public Pelicula CreatePeliculas(@RequestBody Pelicula pelicula) {
+	    return peliculaRepository.save(pelicula);
+	  }
 
 }
