128971885ab8960ec7a48287f83cfe9963873fe0
Erick20320 <erickgarcia20320@gmail.com>
2023-07-01 15:58:01 -0600

    Eliminando y agregando nueva imagen de prueba, Agregando configuracion WebConfig para obtener la ruta de la imagen, y agregando Cors.

---
+++ src/main/java/net/javaguides/springboot/config/WebConfig.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT",
+				"DELETE");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/api/v1/peliculas/img/**").addResourceLocations("classpath:/static/img/");
+	}
+}
