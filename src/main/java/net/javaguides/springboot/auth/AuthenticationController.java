31477d94aa0a1b6586a27a358c29b6963b749f4a
Erick20320 <erickgarcia20320@gmail.com>
2023-07-20 23:17:13 -0600

    Agregando archivos en los siguiente package: auth, config, repository e user. Para asegurar la API con token JWT

---
+++ src/main/java/net/javaguides/springboot/auth/AuthenticationController.java
@@ -0,0 +1,42 @@
+package net.javaguides.springboot.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://192.168.1.24:3000")
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            AuthenticationResponse response = service.register(request);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            String errorMessage = "Error al registrar el usuario: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+        try {
+            AuthenticationResponse response = service.authenticate(request);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            String errorMessage = "Autenticación fallida: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
+        }
+    }
+}
---
+++ src/main/java/net/javaguides/springboot/auth/AuthenticationService.java
@@ -0,0 +1,60 @@
+package net.javaguides.springboot.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import net.javaguides.springboot.config.JwtService;
+import net.javaguides.springboot.repository.UserRepository;
+import net.javaguides.springboot.user.User;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+  private final UserRepository repository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
+  public AuthenticationResponse register(RegisterRequest request) {
+    // Verificar si el correo electrónico ya está en uso
+    if (repository.findByEmail(request.getEmail()).isPresent()) {
+      throw new EmailAlreadyInUseException("El correo electrónico ya está registrado.");
+    }
+
+    var user = User.builder()
+        .firstname(request.getFirstname())
+        .lastname(request.getLastname())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .build();
+    repository.save(user);
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+
+  public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail(),
+            request.getPassword()
+        )
+    );
+    var user = repository.findByEmail(request.getEmail())
+        .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+            .token(jwtToken)
+            .build();
+  }
+
+  public class EmailAlreadyInUseException extends RuntimeException {
+    public EmailAlreadyInUseException(String message) {
+      super(message);
+    }
+  }
+}
